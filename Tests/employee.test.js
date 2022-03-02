8cb6c1a9920c391230a6aa42cfbadcf1ba0a0b6a
farhah din <farhahdin97@hotmail.com>
2022-03-02 17:10:56 +0000

    test for employee

---
+++ Tests/employee.test.js
@@ -0,0 +1,27 @@
+const Employee = require("../Lib/Employee");
+//Test for creaitng a new employee
+test ("Can create a new employee.", () =>{
+const employeeSample = new Employee (name);
+expect (typeof(employeeSample)).toBe("object");
+
+})
+
+/*Test for empoloyee name */
+//const name = "James";
+
+/*Test for empoloyee id */
+//const name = "James";
+
+// Test for email
+// test("Can test email through getEmail method.", () => {
+//     const testEmail = "jamesljenks@gmail.com";
+//     const employeeInstance = new Employee("James", 2, testEmail);
+//     expect(employeeInstance.getEmail()).toBe(testEmail);
+// })
+
+//test for role
+// test("Testing role.", () => {
+//     const returnValue = "Employee";
+//     const employeeInstance = new Employee("James", 2, "jamesljenks@gmail.com");
+//     expect(employeeInstance.getRole()).toBe(returnValue);
+// })
