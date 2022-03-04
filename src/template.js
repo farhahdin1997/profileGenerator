59ac3af754eff7cefac87b53f38907fd3bfd18ec
farhah din <farhahdin97@hotmail.com>
2022-03-04 18:10:53 +0000

    pushing the team on the html

--- src/template.js
+++ src/template.js
@@ -58,5 +58,25 @@ const generateEngineer = engineer => {
 </div>
         `;
     };
-    
+
+    const html = [];
+
+    html.push(team
+        .filter(employee => employee.getRole() === "Manager")
+        .map(manager => generateManager(manager))
+    );
+    html.push(team
+        .filter(employee => employee.getRole() === "Engineer")
+        .map(engineer => generateEngineer(engineer))
+        .join("")
+    );
+    html.push(team
+        .filter(employee => employee.getRole() === "Intern")
+        .map(intern => generateIntern(intern))
+        .join("")
+    );
+
+    return html.join("");
+
 }
+    
