c2945edc2136e7a69c56ef81f2ef9c9e69b370a0
farhah din <farhahdin97@hotmail.com>
2022-03-05 19:58:10 +0000

    started again doing the checks

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,6 +1,5 @@
+// My Attempt
 const Intern = require("../lib/Intern");
-
-//Test for create school
 
 test("Can create school.", () => {
     const testSchool = "School Name";
@@ -8,14 +7,12 @@ test("Can create school.", () => {
     expect(employeeInstance.school).toBe(testSchool);
 });
 
-//Testo for returning office number
 test("Testing officeNumber will return office number.", () => {
     const testSchool = "School Name";
     const employeeInstance = new Intern("James", 2, "jamesljenks@gmail.com", testSchool);
     expect(employeeInstance.getSchool()).toBe(testSchool);
 });
 
-//Testing role
 test("Testing role.", () => {
     const returnValue = "Intern";
     const employeeInstance = new Intern("James", 2, "jamesljenks@gmail.com", "School Name");
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,6 +1,5 @@
-
+// My Attempt
 const Manager = require("../lib/Manager");
-//Test for creating office number
 
 test("Can create an office number.", () => {
     const testOfficeNumber = 2;
@@ -8,14 +7,12 @@ test("Can create an office number.", () => {
     expect(employeeInstance.officeNumber).toBe(testOfficeNumber);
 });
 
-//Test for returning office number
 test("Testing officeNumber will return office number.", () => {
     const testOfficeNumber = 2;
     const employeeInstance = new Manager("James", 2, "jamesljenks@gmail.com", testOfficeNumber);
     expect(employeeInstance.getOfficeNumber()).toBe(testOfficeNumber);
 });
 
-//Testing role
 test("Testing role.", () => {
     const returnValue = "Manager";
     const employeeInstance = new Manager("James", 2, "jamesljenks@gmail.com", 2);
