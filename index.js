e8da08d5f55b331ce7889a15cf87fcfbe76eeec8
farhah din <farhahdin97@hotmail.com>
2022-03-05 19:50:24 +0000

    added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Manager = require("./lib/Manager.js");
-const Engineer = require("./lib/Engineer.js");
+const Engineer = require("./lib/Enginer");
 const Intern = require("./lib/Intern.js");
 const inquirer = require("inquirer");
 const path = require("path");
